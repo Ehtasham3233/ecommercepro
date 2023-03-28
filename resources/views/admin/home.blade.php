5488f3364ba6c58923fbead12aad90aefe7d1bc7
ehtasham3233 <ehtsham3233@gmail.com>
2023-03-29 01:09:31 +0500

    e commerce template added

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Admin Dashboard</title>
-
-    </head>
-
-    <body>
-        <h1>Admin Dashboard</h1>
-    </body>
-</html>
+<x-app-layout>
+   
+</x-app-layout>
